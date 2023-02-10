4670c659bfc71a54a7e015c2ccc44ac677d4a40a
pilsungchoi <vlftjd159753@gmail.com>
2023-02-10 23:32:20 +0900

    first

---
+++ src/users/users.controller.ts
@@ -0,0 +1,10 @@
+import { Body, Controller, Post } from '@nestjs/common';
+import { createUserDto } from './dtos/create-user.dto';
+
+@Controller('auth')
+export class UsersController {
+  @Post('/signup')
+  createUser(@Body() body: createUserDto) {
+    console.log(body);
+  }
+}
