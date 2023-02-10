4f59acb839f443657c57a67ba3689961ac152108
pilsungchoi <vlftjd159753@gmail.com>
2023-02-11 04:29:29 +0900

    service

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -6,4 +6,22 @@ import { User } from './user.entity';
 @Injectable()
 export class UsersService {
   constructor(@InjectRepository(User) private repo: Repository<User>) {}
+
+  create(email: string, password: string) {
+    const user = this.repo.create({ email, password });
+    // create 후 save하는 이유는 entity의 hook들이 실행되냐 안되냐 차이다
+    return this.repo.save(user);
+  }
+
+  findOne(id: number) {
+    return this.repo.findOneBy({ id });
+  }
+
+  find(email: string) {
+    return this.repo.find({ where: { email } });
+  }
+
+  update() {}
+
+  remove() {}
 }
