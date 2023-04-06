da9785857d2be99c20ae04b3e114f2b32ede860b
pilsungchoi <vlftjd159753@gmail.com>
2023-04-06 18:26:14 +0900

    relation

--- src/users/user.entity.ts
+++ src/users/user.entity.ts
@@ -5,7 +5,9 @@ import {
   Entity,
   Column,
   PrimaryGeneratedColumn,
+  OneToMany,
 } from 'typeorm';
+import { Report } from 'src/reports/report.entity';
 
 @Entity()
 export class User {
@@ -17,6 +19,9 @@ export class User {
 
   @Column()
   password: string;
+
+  @OneToMany(() => Report, (report) => report.user) // Does not change the Users table
+  reports: Report[];
 
   @AfterInsert()
   logInsert() {
